dac4c39082718ce9a31df37ce42af981f7ff8a71
KyungEun No <74607521+shruddms@users.noreply.github.com>
2023-01-06 22:37:42 +0900

    Feat : Glide image loading optimization

--- app/src/main/java/com/kyungeun/recyclerview_with_mvvm/ui/drinkdetail/DrinkDetailFragment.kt
+++ app/src/main/java/com/kyungeun/recyclerview_with_mvvm/ui/drinkdetail/DrinkDetailFragment.kt
@@ -1,13 +1,19 @@
 package com.kyungeun.recyclerview_with_mvvm.ui.drinkdetail
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.bumptech.glide.signature.ObjectKey
 import com.kyungeun.recyclerview_with_mvvm.R
 import com.kyungeun.recyclerview_with_mvvm.data.entities.Drink
 import com.kyungeun.recyclerview_with_mvvm.databinding.DrinkDetailFragmentBinding
@@ -18,7 +24,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DrinkDetailFragment : Fragment() {
 
+    private lateinit var layoutListener: ViewTreeObserver.OnGlobalLayoutListener
+
     private var binding: DrinkDetailFragmentBinding by autoCleared()
+
     private val viewModel: DrinkDetailViewModel by viewModels()
 
     override fun onCreateView(
@@ -37,36 +46,69 @@ class DrinkDetailFragment : Fragment() {
     }
 
     private fun setupObservers() {
+
         viewModel.drink.observe(
             viewLifecycleOwner
         ) {
             when (it.status) {
                 Resource.Status.SUCCESS -> {
-                    bindCharacter(it.data!!.results[0])
-                    binding.progressBar.visibility = View.GONE
-                    binding.drinkView.visibility = View.VISIBLE
+                    it.data?.let { _ ->
+                        bindDrinkView(it.data.results[0])
+                    } ?: run {
+                        Toast.makeText(requireContext(), "No data", Toast.LENGTH_SHORT).show()
+                    }
+                    binding.progressBar.visibility = View.INVISIBLE
                 }
                 Resource.Status.ERROR ->
                     Toast.makeText(activity, it.message, Toast.LENGTH_SHORT).show()
 
                 Resource.Status.LOADING -> {
                     binding.progressBar.visibility = View.VISIBLE
-                    binding.drinkView.visibility = View.GONE
+                    binding.drinkView.visibility = View.INVISIBLE
                 }
             }
         }
     }
 
-    private fun bindCharacter(drink: Drink) {
+    private fun bindDrinkView(drink: Drink) {
         binding.category.text = drink.category
         binding.alcoholic.text = drink.alcoholic
         binding.name.text = drink.name
         binding.info.text = drink.info
-        Glide.with(binding.root)
-            .load(drink.image)
-            .override(512, 512)
-            .dontAnimate()
-            .error(R.drawable.error)
-            .into(binding.image)
+
+        layoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+            // Set the signature to be the last modified time of the image file.
+            val imageMetadata = drink.dateModified?.let { ObjectKey(it) } ?: ObjectKey("")
+
+            Glide.with(binding.root)
+                .load(drink.image)
+                .dontAnimate()
+                .error(R.drawable.error)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .signature(imageMetadata)
+                .into(object : CustomTarget<Drawable>(binding.image.width, 1) {
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        binding.drinkView.visibility = View.VISIBLE
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // clear resources
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        transition: Transition<in Drawable>?
+                    ) {
+                        binding.image.setImageDrawable(resource)
+
+                        binding.drinkView.visibility = View.VISIBLE
+
+                        // Remove the listener
+                        binding.image.viewTreeObserver.removeOnGlobalLayoutListener(layoutListener)
+                    }
+
+                })
+        }
+        binding.image.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
     }
 }
